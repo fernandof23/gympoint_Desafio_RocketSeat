8d100ef6e1446189af85a39d57f92f1570e3d8c9
Fernando <fernandorato.0@hotmail.com>
2019-10-16 22:35:26 -0300

    Criada base de pastas

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import routes from './routes';
+
+class App {
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.router();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  router() {
+    this.server.use(routes);
+  }
+}
+
+export default new App();
